77c3a63069c185b4ac1bc0f1e8f05ed8f76269ed
Kiritocris <spiderman4122010@hotmail.com>
2022-03-17 17:50:03 -0600

    Frontendv2

---
+++ utils/index.js
@@ -0,0 +1,10 @@
+window.onload=function(){
+    $('#loading').fadeOut();
+    $('body').removeClass('hidden');
+}
+
+$(document).ready(function() {
+    $("#cf_onclick").click(function() {
+    $("#cf2 img.top").toggleClass("transparent");
+  });
+  });
