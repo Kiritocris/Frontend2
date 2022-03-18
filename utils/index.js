b7f9b17b2d7297243dcb16b4c7368e0273d5137f
Kiritocris <spiderman4122010@hotmail.com>
2022-03-18 03:42:17 -0600

    FrontendVcasifinal

--- utils/index.js
+++ utils/index.js
@@ -2,9 +2,3 @@ window.onload=function(){
     $('#loading').fadeOut();
     $('body').removeClass('hidden');
 }
-
-$(document).ready(function() {
-    $("#cf_onclick").click(function() {
-    $("#cf2 img.top").toggleClass("transparent");
-  });
-  });
